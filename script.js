a4c060e7d5be5f2cdb18f3a57d8ecb13b0327adc
lestermaudiel <flestermaudiel2130@gmail.com>
2023-07-18 09:13:13 -0600

    correccion

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const formulario = document.getElementById('paisForm');
 const tabla = document.querySelector('table');
-const button = document.getElementById('buttonConsulta');
 
 tabla.style.display = 'none';
 
@@ -21,9 +20,9 @@ const consultarPais = async (e) => {
             const data = await respuesta.json();
 
             document.getElementById('nombrePais').innerText = data.name;
-            document.getElementById('CapitalPais').innerText = data.weight;
-            document.getElementById('idiomaPais').innerText = data.types[0].type.name;
-            document.getElementById('imagenBandera').src = data.sprites.front_default;
+            document.getElementById('CapitalPais').innerText = data.capital;
+            document.getElementById('idiomaPais').innerText = data.lang;
+            
             document.getElementById('estado').innerText = 'Pais encontrado';
             tabla.style.display = '';
         } else {
@@ -34,16 +33,3 @@ const consultarPais = async (e) => {
     }
 }
 
-const consultarMetodo = async () => {
-    try {
-        const respuesta = await fetch('./consulta.php');
-        const data = await respuesta.text();
-
-        alert(data);
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-formulario.addEventListener('submit', consultarPais);
-button.addEventListener('click', consultarMetodo);
