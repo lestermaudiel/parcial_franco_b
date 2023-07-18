1ec334bb70254ea011e014c51c8506aaad95fda4
lestermaudiel <flestermaudiel2130@gmail.com>
2023-07-18 09:32:03 -0600

    Se agrego codigo para que se pueda visualizar la bandera.

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ tabla.style.display = 'none';
 
 const consultarPais = async (e) => {
     e.preventDefault();
-    let nombrePais = document.getElementById('paisInput').value;
-    if (nombrePais === '') {
+    let name = document.getElementById('paisInput').value;
+    if (name === '') {
         alert('Debe ingresar el nombre de un país.');
         return;
     }
 
-    const url = `https://restcountries.com/${name}`;
+    const url = `https://restcountries.com/v3.1/name/${name}`;
 
     try {
         const respuesta = await fetch(url);
@@ -19,10 +19,16 @@ const consultarPais = async (e) => {
         if (respuesta.ok) {
             const data = await respuesta.json();
 
-            document.getElementById('nombrePais').innerText = data.name;
-            document.getElementById('capitalPais').innerText = data.capital;
-            document.getElementById('idiomaPais').innerText = data.lang;
-            
+            const pais = data[0];
+
+            document.getElementById('nombrePais').innerText = pais.name.common;
+            document.getElementById('capitalPais').innerText = pais.capital?.[0];
+            document.getElementById('idiomaPais').innerText = Object.values(pais.languages).join(', ');
+
+            const imagenBandera = document.getElementById('imagenBandera');
+            imagenBandera.src = pais.flags.png;
+            imagenBandera.alt = `Bandera de ${pais.name.common}`;
+
             document.getElementById('estado').innerText = 'País encontrado';
             tabla.style.display = '';
         } else {
